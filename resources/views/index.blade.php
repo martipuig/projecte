cc28694c46eace258893039c3aed11a4ad7611f1
marcnovio <marc.novio@edn.cat>
2016-05-30 19:09:11 +0200

    css index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,7 +57,7 @@
 	<div>&nbsp;</div>
 	<div class="container margin-top-slider-movil">
 	    <div class="pull-left text-center" id="ampladaslider">
-	        <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 700px; height: 400px; overflow: hidden; visibility: hidden;">
+	        <div id="jssor_1" style="">
 	            <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 700px; height: 400px; overflow: hidden;">
 	                <div data-p="225.00" style="display: none;">
 	                    <img data-u="image" src="img/s7.jpg" />
@@ -67,14 +67,6 @@
 	                </div>
 	            
 	            </div>
-	            <!-- Bullet Navigator -->
-	            <div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">
-	                <!-- bullet navigator item prototype -->
-	                <div data-u="prototype" style="width:16px;height:16px;"></div>
-	            </div>
-	            <!-- Arrow Navigator -->
-	            <span data-u="arrowleft" class="jssora22l" style="top:0px;left:12px;width:40px;height:58px;" data-autocenter="2"></span>
-	            <span data-u="arrowright" class="jssora22r" style="top:0px;right:12px;width:40px;height:58px;" data-autocenter="2"></span>
 	        </div>
 	        <script>
 	            jssor_1_slider_init();
